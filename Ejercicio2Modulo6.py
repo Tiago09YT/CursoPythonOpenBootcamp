9ef8ebca64f249cd058565de003ba49e796d2b30
Tiago09YT <sanyara2010@gmail.com>
2023-02-27 20:44:08 -0500

    solucuon 6.2

--- Ejercicio2Modulo6.py
+++ Ejercicio2Modulo6.py
@@ -6,11 +6,15 @@ class Alumno:
         self.nota = nota
         #la nota, basada en una escala de 0 a 5
         #aprueba con 3 o mas
-        if (nota>=3):
-            print('Aprobo, su nota fue =',self.nota)
-        else: 
-            print('No aprobo, su nota fue =',self.nota)
-    def __repr__(self):
-        return str(self.__dict__)
+       
+    def imprimir(self):
+            print("Nombre:",self.nombre,";Nota: ", self.nota)
+    def resultado(self):
+            if (self.nota>=3):
+                print('Aprobo, su nota fue =',self.nota)
+            else: 
+                print('No aprobo, su nota fue =',self.nota)
     
 santi = Alumno('Santi', 4)
+santi.imprimir()
+santi.resultado()
