c2b8499f9c6a6ad064401be6811bc9f72ae06fff
Tiago09YT <sanyara2010@gmail.com>
2023-03-01 21:40:23 -0500

    Ejercicio1Modulo10-

--- Ejercicio1Modulo10.py
+++ Ejercicio1Modulo10.py
@@ -32,29 +32,3 @@ clear.grid(column = 1, row = 4,padx = 5,pady=5)
 botonReboot.grid(column = 3,row =4,pady=3, padx =3)
 
 window.mainloop()
-"""from tkinter import *
-
-def seleccionar():
-    monitor.config(text="{}".format(opcion.get()))
-def reset():
-    opcion.set(None)
-    monitor.config(text="")
-
-root = Tk()
-opcion = StringVar()
-opcion.set(None)
-Radiobutton(root, text="Peugeot", variable=opcion, 
-            value='Peugeot', command=seleccionar).pack(anchor=W)
-
-Radiobutton(root, text="Opel", variable=opcion, 
-            value='Opel', command=seleccionar).pack(anchor=W)
-Radiobutton(root, text="Renault", variable=opcion,   
-            value='Renault', command=seleccionar).pack(anchor=W)
-Radiobutton(root, text="Seat", variable=opcion,   
-            value='Seat', command=seleccionar).pack(anchor=W)
-
-monitor = Label(root)
-monitor.pack()
-Button(root, text="Reiniciar", command=reset).pack()
-
-root.mainloop()"""
